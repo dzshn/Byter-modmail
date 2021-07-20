27254b635f8c8ee175473aed52c124c313336a15
Sofia "dzshn" N. L <zshn@pm.me>
2021-07-20 18:34:31 -0300

    save last user instead of setting automatically add setlast command for using last user add message command for giving single messages to someone add reaction for when a message is delivered to someone

--- exts/mail.py
+++ exts/mail.py
@@ -1,14 +1,13 @@
 import discord
 from discord.ext import commands
 
-from . import utils # pylint: disable=relative-beyond-top-level
-
 
 class Mail(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.mod_channel_id = 784136196229955634
-        self.last_dm = None
+        self.last_dm_author = None
+        self.last_msg_author = None
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -17,28 +16,36 @@ class Mail(commands.Cog):
 
         if isinstance(message.channel, discord.DMChannel):
             author = message.author
-            self.last_dm = author
-            out, att = utils.format_output(message.content)
+            self.last_msg_author = author
             await self.bot.get_channel(self.mod_channel_id).send(
                 embed=discord.Embed(
                     color=0x301baa,
                     title=f'Message from {author} ({author.id})',
-                    description=out
-                ),
-                file=att
+                    description=message.content
+                )
             )
 
-        elif self.last_dm is not None and message.channel.id == self.mod_channel_id and not message.content.startswith('m%'):
-            await self.last_dm.send(message.content)
+        elif self.last_dm_author is not None and message.channel.id == self.mod_channel_id and not message.content.startswith('m%'):
+            await self.last_dm_author.send(message.content)
+            await message.add_reaction('📬')
 
     @commands.command(name='set', aliases=['s'])
     async def _set(self, ctx, user: discord.User):
-        self.last_dm = user
+        self.last_dm_author = user
         await ctx.send(f'DM set to {user}')
+
+    @commands.command(aliases=['sl'])
+    async def setlast(self, ctx):
+        self.last_dm_author = self.last_msg_author
+        await ctx.send(f'DM set to {self.last_dm_author}')
+
+    @commands.command(aliases=['m'])
+    async def message(self, ctx, user: discord.User, *, text: str):
+        await user.send(text)
 
     @commands.command(aliases=['c'])
     async def close(self, ctx):
-        self.last_dm = None
+        self.last_dm_author = None
         await ctx.send('Current DM unset')
 
 
